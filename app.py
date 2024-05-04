b9d02a1ebb7751971f2d25fe8208e32ceb72587e
Anton Baht <anton@baht.dk>
2024-05-04 14:58:29 +0200

    .

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 import streamlit as st
 import pandas as pd
-from cs_bahtml.plotting import add_smoke_wall, load_map, add_players
+from cs_bahtml.plotting import add_smoke_wall, add_players
+from cs_bahtml.radars import RADARS
 from math import floor
 
 
 st.set_page_config(layout="wide")
 
-DATA_PATH = './data/'
-MAP_PATH = './media/maps'
 PAGINATION_SIZE = 5
-MAP_NAME = "de_mirage"
 
 def set_map(map, row = None):
-    fig = load_map(MAP_PATH, map)
+    fig = RADARS[map].copy()
     if not row is None:
         side = row['throwerSide']
 
         fig = add_players(fig, [row['throwCords']], map, type = 'thrower')
-        fig = add_smoke_wall(fig, [row['landCords']], map, type = row['grenadeType'])
+        fig = add_smoke_wall(fig, [row['landCords']], map, type = row['grenade_type'])
         fig = add_players(fig, row['players'], map, type = side)
-        fig = add_players(fig, row['dead_players'], map, type = 'dead_'+side)
+        if row['dead_players']:
+            fig = add_players(fig, row['dead_players'], map, type = 'dead_'+side)
     st.session_state.fig = fig
     
 @st.cache_data
 def load_data():
-    df = pd.read_json('./data/parsed_util.json')
+    df = pd.read_json('./data/parsed_clusters.json')
+    return df
+
+@st.cache_data
+def select_team(df, selected):
+    df = df[df.throwerTeam == selected].reset_index()
     df['throwSeconds'] = df['throwSeconds'].apply(int)
-    df_t = df.query('throwerSide=="t"')
-    df_ct = df.query('throwerSide=="ct"')
+    return df
+
+@st.cache_data
+def tab_data(df, map):
+    df = df[df.map_name == map].reset_index()
+    df_t = df.query('throwerSide=="TERRORIST"')
+    df_ct = df.query('throwerSide=="CT"')
     return df_t, df_ct
 
 @st.cache_data
@@ -37,7 +46,7 @@ def split_df(df, rows):
 
 @st.cache_data
 def grouped_list(df):
-    groups = df.groupby(["mapName","throwerSide","throwArea","landArea","grenadeType"]).groups
+    groups = df.groupby(["map_name","throwerSide","grenade_type", "util_score"]).groups
     group_list = [(df.loc[index,:],df.loc[index[0],"util_score"]) for index in groups.values()]
     return [sort[0] for sort in sorted(group_list, key = lambda tup: tup[1])]
 
@@ -46,18 +55,13 @@ def paginator_cb(key, change):
 
 def get_cluster_overview(df, type):
     groups = grouped_list(df)
-    for df_group in groups:
+    for df_group in groups[0:10]:
         n_row = len(df_group)
         score = int(df_group['util_score'].min())
-        print(df_group)
-        util_type = df_group['grenadeType'].iloc[0]
+        util_type = df_group['grenade_type'].iloc[0]
+        if score >900:
+            continue
         if n_row <3:
-            continue
-        if score >1200:
-            continue
-        if (score >1000) & (type=="ct"):
-            continue
-        if not "Heroic" in list(df_group.opponentName):
             continue
 
         
@@ -84,13 +88,13 @@ def get_cluster_overview(df, type):
 
                 # cc[5].text('Lobby/A')
                 # cc[5].text(2)
-                cc[0].dataframe(row[["throwerName", "throwSeconds", "opponentName"]].to_frame().T, hide_index=True, use_container_width=True)
+                cc[0].dataframe(row[["throwerName", "throwSeconds", "opponentTeam"]].to_frame().T, hide_index=True, use_container_width=True)
                 #
 
 
 
                 cc[-1].write("")
-                cc[-1].button(":punch:", key = idx, on_click=set_map, args=(row['mapName'],row))
+                cc[-1].button(":punch:", key = idx, on_click=set_map, args=(row['map_name'],row))
 
 
 
@@ -100,13 +104,21 @@ def get_figure_col():
 def fill_tab(df, type):
     get_cluster_overview(df,type)
             
+def clear_map():
+    del st.session_state.fig
 
 
-df_t, df_ct = load_data()
+st.title('Utility tell tool')
+df = load_data()
+selected = st.selectbox("Select team",df.throwerTeam.unique())
+df = select_team(df, selected)
+map_pick = st.selectbox("Select map",df.map_name.unique(), on_change=clear_map)
+
 if 'fig' not in st.session_state:
-    set_map(MAP_NAME)
+    set_map(map_pick)
 
-st.title('Vitality on mirage utility tell tool')
+df_t, df_ct = tab_data(df, map_pick)
+
 cluster_col, fig_col =st.columns(2)
 
 with cluster_col:
@@ -122,5 +134,3 @@ with cluster_col:
     
 with fig_col:
     get_figure_col()
-
-
